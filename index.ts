befe2a08ad89bf6661e13bf1778c53c492ad02e2
Asmarah <asmarahsyed1986@gmail.com>
2024-03-25 21:23:25 +0500

    Adding a dream-number-guessing-game

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\tWEllcome to my dream-number-guessing-game\n");
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+    name : "userGuessedNumber",
+    type : "number",
+    message : "Enter your guess number between 1-6:",
+},
+])
+
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congratulations! You guess a correct number.");
+    
+}else{
+    console.log("Sorry, You guess a wrong numbe.");
+    
+}
